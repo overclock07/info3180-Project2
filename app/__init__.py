cced7fe8c8245e9df227fcd46604c55ced9f948b
overclock07 <sunjen08@gmail.com>
2018-05-09 06:28:33 +0000

    Part 2 done

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ login_manager.login_view = 'login'
 login_manager.session_protection = "strong"
 
 UPLOAD_FOLDER = './app/static/uploads'
-DATABASE_URL = 'postgresql://user[:password]@localhost/photogram'
+DATABASE_URL = 'postgresql://leqzxcwgstchtl:e3702aaa318aa8a48c170baa2bbb0f3b238d8ea8752a5d69c6ddaee47f8d30a4@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d3uuk205jb47lq'
+#DATABASE_URL = 'postgresql://user[:password]@localhost/photogram'
 
 app.config['SECRET_KEY'] = 'Its a secret'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
